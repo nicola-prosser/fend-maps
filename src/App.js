3e27478e0ff3a1113e979557b877ce294a731126
Nicola-Prosser <nicola.prosser@outlook.com>
2018-08-06 20:22:42 +0100

    infowindows implemented

--- src/App.js
+++ src/App.js
@@ -8,8 +8,7 @@ const artLocations = [
     id: 1,
     name: "Centre for Contemporary Arts",
     image: {
-      src:
-        "https://www.archdaily.com/898009/glasgow-school-of-art-to-be-rebuilt-according-to-schools-director",
+      src: "//c1.staticflickr.com/4/3167/2946870420_bba3e680de_b.jpg",
       alt: "Art school"
     },
     location: {
@@ -21,8 +20,7 @@ const artLocations = [
     id: 2,
     name: "Glasgow School of Art",
     image: {
-      src:
-        "https://www.archdaily.com/898009/glasgow-school-of-art-to-be-rebuilt-according-to-schools-director",
+      src: "//c1.staticflickr.com/4/3167/2946870420_bba3e680de_b.jpg",
       alt: "Art school"
     },
     location: {
@@ -34,8 +32,7 @@ const artLocations = [
     id: 3,
     name: "Modern Institute",
     image: {
-      src:
-        "https://www.archdaily.com/898009/glasgow-school-of-art-to-be-rebuilt-according-to-schools-director",
+      src: "//c1.staticflickr.com/4/3167/2946870420_bba3e680de_b.jpg",
       alt: "Art school"
     },
     location: {
@@ -47,8 +44,7 @@ const artLocations = [
     id: 4,
     name: "Gallery of Modern Art",
     image: {
-      src:
-        "https://www.archdaily.com/898009/glasgow-school-of-art-to-be-rebuilt-according-to-schools-director",
+      src: "//c1.staticflickr.com/4/3167/2946870420_bba3e680de_b.jpg",
       alt: "Art school"
     },
     location: {
@@ -60,8 +56,7 @@ const artLocations = [
     id: 5,
     name: "Lighthouse",
     image: {
-      src:
-        "https://www.archdaily.com/898009/glasgow-school-of-art-to-be-rebuilt-according-to-schools-director",
+      src: "//c1.staticflickr.com/4/3167/2946870420_bba3e680de_b.jpg",
       alt: "Art school"
     },
     location: {
@@ -73,45 +68,49 @@ const artLocations = [
     id: 6,
     name: "The Old Hairdressers",
     image: {
-      src:
-        "https://www.archdaily.com/898009/glasgow-school-of-art-to-be-rebuilt-according-to-schools-director",
+      src: "//c1.staticflickr.com/4/3167/2946870420_bba3e680de_b.jpg",
       alt: "Art school"
     },
-    lat: 55.861009,
-    lng: -4.258023
+    location: {
+      lat: 55.861009,
+      lng: -4.258023
+    }
   },
   {
     id: 7,
     name: "Good Press",
     image: {
-      src:
-        "https://www.archdaily.com/898009/glasgow-school-of-art-to-be-rebuilt-according-to-schools-director",
+      src: "//c1.staticflickr.com/4/3167/2946870420_bba3e680de_b.jpg",
       alt: "Art school"
     },
-    lat: 55.854691,
-    lng: -4.246533
+    location: {
+      lat: 55.854691,
+      lng: -4.246533
+    }
   },
   {
     id: 8,
     name: "The Glasgow Art Club",
     image: {
-      src:
-        "https://www.archdaily.com/898009/glasgow-school-of-art-to-be-rebuilt-according-to-schools-director",
+      src: "//c1.staticflickr.com/4/3167/2946870420_bba3e680de_b.jpg",
       alt: "Art school"
     },
-    lat: 55.864453,
-    lng: -4.262676
+    location: {
+      lat: 55.864453,
+      lng: -4.262676
+    }
   },
   {
     id: 9,
     name: "Trongate 103",
     image: {
-      src:
-        "https://www.archdaily.com/898009/glasgow-school-of-art-to-be-rebuilt-according-to-schools-director",
+      src: "//c1.staticflickr.com/4/3167/2946870420_bba3e680de_b.jpg",
       alt: "Art school"
     },
-    lat: 55.856826,
-    lng: -4.246819
+    location: {
+      lat: 55.856826,
+      lng: -4.246819
+    }
   }
 ];
 
--- src/GlasgowMap.js
+++ src/GlasgowMap.js
@@ -1,16 +1,42 @@
 import React, { Component } from "react";
-import GoogleMapReact from "google-map-react";
+//import GoogleMapReact from "google-map-react";
 import "./GlasgowMap.css";
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
 
 //'https://maps.googleapis.com/maps/api/js?key=AIzaSyA129edK8SHKOZZTNlznzqYF8zuNfSY6Pg&callback=initMap'
 
+const Markers = props =>
+  props.artLocations.map(marker => (
+    <Marker
+      {...props}
+      key={marker.id}
+      title={marker.name}
+      name={marker.name}
+      position={marker.location}
+      onClick={props.onMarkerClick}
+      image={marker.image}
+    />
+  ));
+
 export class GlasgowMap extends Component {
   static defaultMap = {
     center: { lat: 55.86116, lng: -4.253887 },
     zoom: 13
-    //styles: styles
   };
+
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {}
+  };
+
+  handleMarkerClick = (props, marker, e) =>
+    console.log(props.image) ||
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
 
   render() {
     return (
@@ -24,14 +50,23 @@ export class GlasgowMap extends Component {
           lng: -4.253887
         }}
       >
-        {this.props.artLocations.map(marker => (
-          <Marker
-            key={marker.id}
-            title={marker.name}
-            name={marker.name}
-            position={marker.location}
-          />
-        ))}
+        <Markers {...this.props} onMarkerClick={this.handleMarkerClick} />
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+        >
+          <div>
+            <h2>{this.state.selectedPlace.name}</h2>
+            <div>
+              {this.state.selectedPlace.image && (
+                <img
+                  src={this.state.selectedPlace.image.src}
+                  alt={this.state.selectedPlace.image.alt}
+                />
+              )}
+            </div>
+          </div>
+        </InfoWindow>
       </Map>
     );
   }
